b6717f370e976a3bdf86a8455caebd390d3245d2
tomatosoup47 <114050857+tomatosoup47@users.noreply.github.com>
2022-09-21 09:08:36 +0300

    Update rpsgame.py

--- rpsgame.py
+++ rpsgame.py
@@ -1,4 +1,27 @@
+```py
+
 human_turn = 'rock'
+
 computer_turn = 'scissors'
 
-if
+
+
+if human_turn == computer_turn:
+
+print('Draw!')
+
+elif human_turn == 'rock' and computer_turn == 'scissors':
+
+print('Human wins!')
+
+elif human_turn == 'paper' and computer_turn == 'rock':
+
+print('Human wins!')
+
+elif human_turn == 'scissors' and computer_turn == 'paper':
+
+print('Human wins!')
+
+else:
+
+print('Computer wins!')```
